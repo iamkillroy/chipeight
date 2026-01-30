b48ebc4d089a08ebb9f265bd752f9ebb67e6b803
Frias <l382f965@ENGR1005-06.home.ku.edu>
2026-01-30 08:52:06 -0600

    Implemented initial graphics, X11 dependecies required

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
+
+import (
+    "github.com/gopxl/pixel/v2"
+    "fmt"
+    "github.com/gopxl/pixel/v2/backends/opengl"
+)
+
 //By Lucas Frias KU EECS 268
-# Lab 1 - Dated Jan 27th 2026
-#       <@
-#       (KU//
-#        "
-# Rock Chalk!
-# Home grown, no AI code with lots of comments
-import "fmt"
+// Lab 1 - Dated Jan 27th 2026
+//       <@
+//       (KU//
+//        "
+// Rock Chalk!
+// Home grown, no AI code with lots of comments
 
 func main(){
 	fmt.Println("");
+	opengl.Run(run);
 }
